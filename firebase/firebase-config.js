10c81076b9204c17f8b617432c00a2ce7578a887
WETrendingTeam <lade.galleria@gmail.com>
2026-08-24 17:10:31 +0100

    Add files via upload

---
+++ firebase/firebase-config.js
@@ -0,0 +1,3 @@
+// Compatibility entry point for older BALAD files.
+// The live Firebase configuration is maintained in ../firebase-config.js.
+export { app, db, auth, getBaladMessaging } from "../firebase-config.js";
